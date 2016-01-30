d70049fc784b69176002deeaf12b5a02f4a8773f
Marcel <marcel.hofgesang@live.de>
2016-01-30 13:27:47 +0100

    trapBalllvl6

---
+++ Cube Town/Assets/Scripts/moveBall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveBall : MonoBehaviour {
+    
+    Vector3 pos;
+    float spawned;
+    float halfIntervall;
+    public float intervall = 2;
+    public float offset = 0;
+    public int distX = 8;
+    public int distY = 0;
+    public int distZ = 0;
+    
+    // Use this for initialization
+    void Start () {
+        halfIntervall = intervall / 2;
+        pos = transform.position;
+        spawned = Time.realtimeSinceStartup - offset*intervall;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        float time = Time.realtimeSinceStartup-spawned;
+        time %= halfIntervall*2;
+        if (time > halfIntervall)
+        {
+            time = 2*halfIntervall - time;
+        }
+        time /= halfIntervall;
+        
+        transform.position = new Vector3(pos.x + distX*time, pos.y + distY *time, pos.z + distZ *time);
+    }
+}
