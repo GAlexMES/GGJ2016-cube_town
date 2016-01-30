6267a03426b9b57d0a1ca8284f288791512e725d
Marcel <marcel.hofgesang@live.de>
2016-01-30 14:13:20 +0100

    no risk no fun

---
+++ Cube Town/Assets/Scripts/DeathTriger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTriger : MonoBehaviour {
+
+	
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("bam");
+        ResetScene.reset();
+    }
+}
