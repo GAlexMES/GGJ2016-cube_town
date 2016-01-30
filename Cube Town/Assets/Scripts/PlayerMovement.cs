731e7d5c0cb0a196f06da7e5e4ea941742f4c9f0
haloshat <till.schlechtweg@gmail.com>
2016-01-30 11:55:05 +0100

    extra PlayerMovement for you

--- Cube Town/Assets/Scripts/PlayerMovement.cs
+++ Cube Town/Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
+    public float gravity = 10.0F;
+    
+    public GameObject[] objs;
+
     bool moved = false;
     Vector3 start;
 
     CharacterController controller;
     private Vector3 moveDirection = Vector3.zero;
+
+    AudioSource source;
+    GameObject cam;
 
     // Use this for initialization
     void Start()
@@ -20,26 +26,36 @@ public class PlayerMovement : MonoBehaviour
 
         //Get the Starting position of the Player
         start = transform.position;
+        source = GetComponent<AudioSource>();
+        cam = GameObject.Find("Main Camera");
     }
 
     //This Function gets called every frame
     void Update()
     {
         //Reset to start, if the player has fallen down
-        if(transform.position.y < -2)
+        if(transform.position.y < -10)
         {
             transform.position = start;
         }
 
-        moveDirection = new Vector3(0, 0, 0);
+        moveDirection = Vector3.zero;
 
-        if (controller.isGrounded && !moved)
+        if (!moved)
         {
             if (Input.GetKey(KeyCode.DownArrow))
             {
                 moveDirection = new Vector3(1, 0, 0);
                 moved = true;
-
+                if(objs.Length > 0)
+                {
+                    foreach(GameObject obj in objs)
+                    {
+                        if (obj.transform.position.Equals(new Vector3(transform.position.x + 1.0F, transform.position.y, transform.position.z))){
+                            obj.transform.position = new Vector3(obj.transform.position.x + 1.0F, obj.transform.position.y, obj.transform.position.z);
+                        }
+                    }
+                }
             }
             else if (Input.GetKey(KeyCode.UpArrow))
             {
@@ -56,7 +72,7 @@ public class PlayerMovement : MonoBehaviour
                 moveDirection = new Vector3(0, 0, -1);
                 moved = true;
             }
-            moveDirection = transform.TransformDirection(moveDirection);
+            source.Play();
         }
         else
         {
@@ -66,7 +82,9 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection);
+        //moveDirection.y -= gravity * Time.deltaTime;
+        moveDirection = transform.TransformDirection(moveDirection);
+        transform.position = transform.position + moveDirection;
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, transform.position.z);
     }
 }
