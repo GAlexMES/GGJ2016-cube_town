f879d86544088b5c78fb930111f41c643e033dbe
Alexander Brennecke <alexander.brenneck@gmail.com>
2016-01-31 12:56:10 +0100

    merge

--- Cube Town/Assets/Scripts/CameraTracking.cs
+++ Cube Town/Assets/Scripts/CameraTracking.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class CameraTracking : MonoBehaviour {
 
     GameObject cam;
-    GameObject player;
+    public float camDistance = 100;
+    public float camAngle = 45;
+    public Vector3 up = new Vector3(0, 1, 0);
+
+
     // Use this for initialization
     void Start () {
         cam = GameObject.Find("Main Camera");
@@ -13,6 +17,13 @@ public class CameraTracking : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, player.transform.position.z);
+        float angle = camAngle * Mathf.Deg2Rad;
+        Vector3 direction = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+        Vector3 camPos = camDistance * direction;
+        camPos = transform.TransformPoint(camPos);
+        cam.transform.position = camPos;
+        Quaternion camRot = Quaternion.LookRotation(-transform.TransformDirection(direction), up);
+        //Quaternion camRot = Quaternion.LookRotation(-camPos, up);
+        cam.transform.rotation = camRot;
     }
 }
