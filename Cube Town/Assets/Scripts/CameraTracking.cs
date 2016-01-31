0582fc7ccc1efc9fcad3b538968a780b9896f3b0
Alexander Brennecke <alexander.brenneck@gmail.com>
2016-01-31 12:51:39 +0100

    updated levels

--- Cube Town/Assets/Scripts/CameraTracking.cs
+++ Cube Town/Assets/Scripts/CameraTracking.cs
@@ -4,16 +4,15 @@ using System.Collections;
 public class CameraTracking : MonoBehaviour {
 
     GameObject cam;
-
+    GameObject player;
     // Use this for initialization
     void Start () {
-
         cam = GameObject.Find("Main Camera");
+        player = GameObject.Find("Player");
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, transform.position.z);
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, player.transform.position.z);
     }
 }
